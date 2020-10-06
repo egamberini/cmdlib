a33c52cd40e1db16e143b19379d1b60419fc64ef
roland-sipos <sipos.roland.cb@gmail.com>
2020-10-06 14:29:58 +0200

    Base classes

---
+++ include/CommandFacility.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file CommandFacility.hpp CommandFacility interface
+ */
+#ifndef CMDLIB_INCLUDE_COMMANDFACILITY_HPP_
+#define CMDLIB_INCLUDE_COMMANDFACILITY_HPP_
+
+namespace dune::daq::ccm {
+
+/**
+ * @brief Interface needed by DAQ apps and services for command handling
+ */
+class CommandFacility
+{
+public:
+protected:
+private:
+};
+
+}
+
+#endif // CMDLIB_INCLUDE_COMMANDFACILITY_HPP_
---
+++ include/CommandedObject.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file CommandedObject.hpp CommandedObject interface
+ */
+#ifndef CMDLIB_INCLUDE_COMMANDEDOBJECT_HPP_
+#define CMDLIB_INCLUDE_COMMANDEDOBJECT_HPP_
+
+namespace dune::daq::ccm {
+
+/**
+ * @brief Interface needed by commanded objects in the DAQ
+ */
+class CommandedObject
+{
+public:
+protected:
+private:
+};
+
+}
+
+#endif // CMDLIB_INCLUDE_COMMANDEDOBJECT_HPP_
