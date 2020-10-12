86eb6e7a2539ba9a60ab1381c010ed5f8157a9e4
roland-sipos <sipos.roland.cb@gmail.com>
2020-10-12 17:42:28 +0200

    Move second basic command facility to cmdlib library

--- include/cmdlib/Issues.hpp
+++ include/cmdlib/Issues.hpp
@@ -22,6 +22,42 @@ namespace dunedaq {
                       ((std::string)error)
                       ((std::string)uri))
 
+    // Throw if a URI is given which is not supported by the code.
+    // Provide the erroneous URI as arg.
+    ERS_DECLARE_ISSUE(cmdlib, UnsupportedUri,
+                      "Unsupported URI: " << uri,
+                      ((std::string)uri))
+
+    // Throw if a file can not be opened.  Provide "mode" of "reading"
+    // or "writing" and provide erroneous filename as args.
+    ERS_DECLARE_ISSUE(cmdlib, BadFile,
+                      "Can not open file for " << mode << ": " << filename,
+                      ((std::string)filename)
+                      ((std::string)mode))
+
+    // Throw if a stream of data is exhausted.  Use in methods that
+    // are given semantics that imply the stream is not expected to
+    // become exhausted.  Ie, GetNext() may throw but something like
+    // GetNext(return_if_exhausted) -> value must not.  Name the
+    // stream and describe the error in the args.
+    ERS_DECLARE_ISSUE(cmdlib, StreamExhausted,
+                      "Stream exhausted: " << stream_name << " err: " << err,
+                      ((std::string)stream_name)
+                      ((std::string)err))
+
+    // Throw if a stream of data being consumed has values which are
+    // not expected.  Give the stream name and error message as args.
+    ERS_DECLARE_ISSUE(cmdlib, StreamCorrupt,
+                      "Stream corrupt: " << stream_name << " err: " << err,
+                      ((std::string)stream_name)
+                      ((std::string)err))                  
+
+    // Throw if an operation is the result of logically inconsistent
+    // code.  Describe the error in the "what" arg.
+    ERS_DECLARE_ISSUE(cmdlib, InternalError,
+                      "Internal error: " << what,
+                      ((std::string)what))    
+
     ERS_DECLARE_ISSUE(cmdlib, CommandParserError,
                       "Failed to parse command: " << error,
                       ((std::string)error))
